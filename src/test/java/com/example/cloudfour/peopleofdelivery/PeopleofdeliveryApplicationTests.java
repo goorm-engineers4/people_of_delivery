7d351f2beb21962215d99eab4a88b10772b36e32
JUNE <jh010303@naver.com>
2025-07-24 23:17:12 +0900

    :rocket: 스프링 프로젝트 생성
    - postgresql 연결
    - application.yml 생성, 환경변수 사용

---
+++ src/test/java/com/example/cloudfour/peopleofdelivery/PeopleofdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudfour.peopleofdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleofdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
