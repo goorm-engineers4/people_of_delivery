102d78d72e03d330936cc116394cda5183beaea8
JIYOOnii <rlawldbs1007@gmail.com>
2025-07-25 16:50:36 +0900

    ✨Feat: 가게 엔터티 생성 - Store 도메인 엔터티 클래스 생성 (UUID 기반) - NOT NULL 제약 조건 및 컬럼 설정 적용 - @builder 사용으로 빌더 패턴 지원
    PR전 추가 수정4
    - (수정1에서)복수로 수정한 파일 및 폴더명을 다시 단수로 수정
    - stores -> store

---
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/store/entity/Store.java
@@ -0,0 +1,81 @@
+package com.example.cloudfour.peopleofdelivery.domain.store.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Store {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private UUID storeId;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private UUID regionID;
+
+    @Column(nullable = false)
+    private UUID storeCategoriesId;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String address;
+
+    private String storePicture;
+
+    @Column(nullable = false, length = 255)
+    private String phone;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private Integer minPrice;
+
+    @Column(nullable = false)
+    private Integer deliveryTip;
+
+    private Float rating;
+
+    private Integer likeCount;
+
+    private Integer reviewCount;
+
+    @Column(nullable = false, length = 255)
+    private String operationHours;
+
+    @Column(nullable = false, length = 255)
+    private String closedDays;
+
+    @Builder
+    public Store(UUID userId, UUID regionID, UUID storeCategoriesId, String name, String address,
+                 String storePicture, String phone, String content, Integer minPrice, Integer deliveryTip,
+                 Float rating, Integer likeCount, Integer reviewCount, String operationHours,
+                 String closedDays) {
+        this.userId = userId;
+        this.regionID = regionID;
+        this.storeCategoriesId = storeCategoriesId;
+        this.name = name;
+        this.address = address;
+        this.storePicture = storePicture;
+        this.phone = phone;
+        this.content = content;
+        this.minPrice = minPrice;
+        this.deliveryTip = deliveryTip;
+        this.rating = rating;
+        this.likeCount = likeCount;
+        this.reviewCount = reviewCount;
+        this.operationHours = operationHours;
+        this.closedDays = closedDays;
+    }
+}
