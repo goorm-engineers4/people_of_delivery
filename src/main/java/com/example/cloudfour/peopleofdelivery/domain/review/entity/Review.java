a36d0387faa153fcf6dbc495da4701b450ce7317
JUNE <jh010303@naver.com>
2025-07-26 22:07:15 +0900

    :sparkles: feat: Review 엔티티 연관관계 매핑
    - 연관관계 매핑, 연관관계 메서드 구현
    - builder의 id 메소드 재정의 하여 외부에서 수정 못하도록 보장

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/review/entity/Review.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/review/entity/Review.java
@@ -1,18 +1,18 @@
 package com.example.cloudfour.peopleofdelivery.domain.review.entity;
 
+import com.example.cloudfour.peopleofdelivery.domain.menu.entity.Menu;
+import com.example.cloudfour.peopleofdelivery.domain.store.entity.Store;
 import com.example.cloudfour.peopleofdelivery.domain.user.entity.User;
 import com.example.cloudfour.peopleofdelivery.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
-
 import java.util.UUID;
 
 @Entity
 @Getter
-@SuperBuilder
+@Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "p_review")
 public class Review extends BaseEntity {
     @Id
@@ -31,4 +31,33 @@ public class Review extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", nullable = false)
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menuId", nullable = false)
+    private Menu menu;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "storeId", nullable = false)
+    private Store store;
+
+    public static class ReviewBuilder{
+        private ReviewBuilder id(UUID id){
+            throw new UnsupportedOperationException("id 수동 생성 불가");
+        }
+    }
+
+    public void setUser(User user){
+        this.user = user;
+        user.getReviews().add(this);
+    }
+
+    public void setMenu(Menu menu){
+        this.menu = menu;
+        menu.getReviews().add(this);
+    }
+
+    public void setStore(Store store){
+        this.store = store;
+        store.getReviews().add(this);
+    }
 }
