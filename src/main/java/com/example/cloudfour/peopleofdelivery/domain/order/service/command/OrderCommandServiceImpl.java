cbe2fabfb28b7026075115e9275d6fb389158b0b
JUNE <jh010303@naver.com>
2025-07-29 16:08:08 +0900

    :sparkles: feat: TDD 구현을 위한 최소한의 Service 생성
    - 삭제, 생성, 수정 서비스

---
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/order/service/command/OrderCommandServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.cloudfour.peopleofdelivery.domain.order.service.command;
+
+import com.example.cloudfour.peopleofdelivery.domain.order.dto.OrderRequestDTO;
+import com.example.cloudfour.peopleofdelivery.domain.order.dto.OrderResponseDTO;
+import com.example.cloudfour.peopleofdelivery.domain.user.entity.User;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderCommandServiceImpl implements OrderCommandService {
+    @Override
+    public OrderResponseDTO.OrderCreateResponseDTO createOrder(OrderRequestDTO.OrderCreateRequestDTO orderCreateRequestDTO, User user) {
+        return null;
+    }
+
+    @Override
+    public OrderResponseDTO.OrderUpdateResponseDTO updateOrder(OrderRequestDTO.OrderUpdateRequestDTO orderUpdateRequestDTO, UUID orderId, User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(UUID orderId, User user) {
+
+    }
+
+}
