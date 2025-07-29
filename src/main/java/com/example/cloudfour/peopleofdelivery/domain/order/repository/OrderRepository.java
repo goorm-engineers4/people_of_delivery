a06430dbd7a6a762710e6b3295bf36646dbb2b5c
JUNE <jh010303@naver.com>
2025-07-29 16:20:22 +0900

    :sparkles: feat: 주문 리포지터리 생성

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/order/repository/OrderRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, UUID> {
     Optional<Order> findById(UUID orderId);
 }
