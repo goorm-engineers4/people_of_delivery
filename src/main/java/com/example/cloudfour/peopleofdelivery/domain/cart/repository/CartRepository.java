8ed775ef61c81fe5dda9dc53d9f69ded5b59a630
sungchilll <csk2751@gmail.com>
2025-07-30 10:10:23 +0900

    ✨feat: 장바구니 Repository 생성
    - 장바구니 Repository JPA 추가

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/cart/repository/CartRepository.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/cart/repository/CartRepository.java
@@ -1,4 +1,9 @@
 package com.example.cloudfour.peopleofdelivery.domain.cart.repository;
 
-public interface CartRepository {
+import com.example.cloudfour.peopleofdelivery.domain.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CartRepository extends JpaRepository<Cart, UUID> {
 }
