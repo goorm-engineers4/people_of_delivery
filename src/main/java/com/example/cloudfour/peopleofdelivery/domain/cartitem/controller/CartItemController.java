c84f77f6111ba66f10fc78e898a103aa7793c91b
sungchilll <csk2751@gmail.com>
2025-07-30 10:30:34 +0900

    🐛fix: endpoint 수정

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/cartitem/controller/CartItemController.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/cartitem/controller/CartItemController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/cartItem")
+@RequestMapping("/api/cartItems")
 @RequiredArgsConstructor
 public class CartItemController {
     private final CartItemCommandService cartItemCommandService;
