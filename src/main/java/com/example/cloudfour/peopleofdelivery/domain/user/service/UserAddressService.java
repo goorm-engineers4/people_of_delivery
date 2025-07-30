a8069a51506e25b5a35cc120818758dc76235db3
shieun <shieunmo@gmail.com>
2025-07-30 12:59:24 +0900

    ✨ feat: 최소한의 UserService, UserAddressService 구현 - UserController에 필요한 UserService, UserAddressService 작성

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/user/service/UserAddressService.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/user/service/UserAddressService.java
@@ -1,4 +1,31 @@
 package com.example.cloudfour.peopleofdelivery.domain.user.service;
 
+import com.example.cloudfour.peopleofdelivery.domain.user.dto.UserRequestDTO;
+import com.example.cloudfour.peopleofdelivery.domain.user.dto.UserResponseDTO;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class UserAddressService {
+    public void addAddress(UUID userId, UserRequestDTO.AddressRequestDTO request) {
+        // Logic to add an address for the user
+    }
+
+    public List<UserResponseDTO.AddressResponseDTO> getAddresses(UUID userId) {
+        // Logic to retrieve the user's address
+        return null;
+    }
+    public void updateAddress(UUID userId, UUID addressId, UserRequestDTO.AddressRequestDTO request) {
+        // Logic to update the user's addres
+    }
+
+    public void softDeleteAddress(UUID userId, UUID addressId) {
+        // Logic to delete the user's address
+    }
 }
