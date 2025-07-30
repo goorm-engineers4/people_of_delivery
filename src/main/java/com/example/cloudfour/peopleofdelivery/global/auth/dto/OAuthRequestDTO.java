e2e456d4c0430f9aee2e5d8e7fe6a4ad2d1162c5
shieun <shieunmo@gmail.com>
2025-07-30 15:00:44 +0900

    🔨 refactor : oauth 수정 - request dto 내 어노테이션 제거 - service builder email 추가

--- src/main/java/com/example/cloudfour/peopleofdelivery/global/auth/dto/OAuthRequestDTO.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/global/auth/dto/OAuthRequestDTO.java
@@ -1,27 +1,16 @@
 package com.example.cloudfour.peopleofdelivery.global.auth.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 public class OAuthRequestDTO {
     @Getter
     @Builder
-    public class AdditionalSignupRequestDto {
-        @Email
-        @NotBlank
+    public static class AdditionalSignupRequestDto {
         String email;
-
-        @NotBlank
         String number;
-
-        @NotBlank
         String nickname;
-
-        @NotBlank
         String providerId; // OAuth2 'sub' 값
     }
 }
