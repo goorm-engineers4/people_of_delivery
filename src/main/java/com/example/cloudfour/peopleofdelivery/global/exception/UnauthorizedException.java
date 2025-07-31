7bb3a5c152da6d25b83e42dc786a2c0568adf482
ByeongminJeong <hegassss@gmail.com>
2025-07-31 14:31:20 +0900

    🔨 refactor: 글로벌 예외 처리 삭제

--- src/main/java/com/example/cloudfour/peopleofdelivery/global/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cloudfour.peopleofdelivery.global.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
