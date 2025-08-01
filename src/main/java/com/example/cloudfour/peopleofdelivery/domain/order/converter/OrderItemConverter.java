eb8598fb858cac5358df5ba77272b7f9d817d637
JUNE <jh010303@naver.com>
2025-08-01 17:02:34 +0900

    :sparkles: feat: orderItem 컨버터 기능 추가
    - 장바구니 아이템을 주문 아이템으로 복제하는 메서드 추가

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/order/converter/OrderItemConverter.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/order/converter/OrderItemConverter.java
@@ -1,18 +1,32 @@
 package com.example.cloudfour.peopleofdelivery.domain.order.converter;
 
-import com.example.cloudfour.peopleofdelivery.domain.menu.entity.MenuOption;
+import com.example.cloudfour.peopleofdelivery.domain.cartitem.entity.CartItem;
+import com.example.cloudfour.peopleofdelivery.domain.menu.dto.MenuOptionResponseDTO;
 import com.example.cloudfour.peopleofdelivery.domain.order.dto.OrderItemResponseDTO;
+import com.example.cloudfour.peopleofdelivery.domain.order.entity.Order;
 import com.example.cloudfour.peopleofdelivery.domain.order.entity.OrderItem;
 
 import java.util.List;
 
 public class OrderItemConverter {
-    public static OrderItemResponseDTO.OrderItemListResponseDTO toOrderItemClassListDTO(OrderItem orderItem, List<MenuOption> menuOption) {
+    public static OrderItemResponseDTO.OrderItemListResponseDTO toOrderItemClassListDTO(OrderItem orderItem, List<MenuOptionResponseDTO.MenuOptionListResponseDTO> menuOption) {
         return OrderItemResponseDTO.OrderItemListResponseDTO.builder()
                 .optionList(menuOption)
                 .price(orderItem.getPrice())
                 .quantity(orderItem.getQuantity())
                 .menuName(orderItem.getMenu().getName())
                 .build();
+    }
+
+    public static OrderItem CartItemtoOrderItem(CartItem cartItem, Order order) {
+        OrderItem orderItem = OrderItem.builder()
+                .quantity(cartItem.getQuantity())
+                .price(cartItem.getPrice())
+                .build();
+        orderItem.setMenu(cartItem.getMenu());
+        orderItem.setOrder(order);
+        orderItem.setMenuOption(cartItem.getMenuOption());
+        return orderItem;
+
     }
 }
