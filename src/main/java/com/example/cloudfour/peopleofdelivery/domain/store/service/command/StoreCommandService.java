5b1a87a59d88275833dd8330a9471900b37b73d6
JUNE <jh010303@naver.com>
2025-08-02 17:25:17 +0900

    :sparkles: feat: 수정/생성 서비스 알고리즘 변경
    - 생성할 때 가게 카테고리를 입력받고 가게 카테고리, 가게 생성하도록 변경
    - 이미 가게 카테고리가 존재하면 존재하는 카테고리 사용
    - 수정 알고리즘도 마찬가지
    - region 기능 미완성에 따라 적용 X

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/store/service/command/StoreCommandService.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/store/service/command/StoreCommandService.java
@@ -2,6 +2,7 @@ package com.example.cloudfour.peopleofdelivery.domain.store.service.command;
 
 import com.example.cloudfour.peopleofdelivery.domain.region.entity.Region;
 import com.example.cloudfour.peopleofdelivery.domain.region.repository.RegionRepository;
+import com.example.cloudfour.peopleofdelivery.domain.store.converter.StoreCategoryConverter;
 import com.example.cloudfour.peopleofdelivery.domain.store.converter.StoreConverter;
 import com.example.cloudfour.peopleofdelivery.domain.store.dto.StoreRequestDTO;
 import com.example.cloudfour.peopleofdelivery.domain.store.dto.StoreResponseDTO;
@@ -13,11 +14,17 @@ import com.example.cloudfour.peopleofdelivery.domain.store.repository.StoreCateg
 import com.example.cloudfour.peopleofdelivery.domain.store.repository.StoreRepository;
 import com.example.cloudfour.peopleofdelivery.domain.user.entity.User;
 import com.example.cloudfour.peopleofdelivery.domain.user.enums.Role;
+import com.example.cloudfour.peopleofdelivery.domain.user.exception.UserErrorCode;
+import com.example.cloudfour.peopleofdelivery.domain.user.exception.UserException;
+import com.example.cloudfour.peopleofdelivery.domain.user.repository.UserRepository;
+import com.example.cloudfour.peopleofdelivery.global.auth.userdetails.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class StoreCommandService {
@@ -25,54 +32,68 @@ public class StoreCommandService {
     private final StoreRepository storeRepository;
     private final RegionRepository regionRepository;
     private final StoreCategoryRepository storeCategoryRepository;
+    private final UserRepository userRepository;
 
-    public StoreResponseDTO.StoreCreateResponseDTO createStore(StoreRequestDTO.StoreCreateRequestDTO dto, User user) {
+    public StoreResponseDTO.StoreCreateResponseDTO createStore(StoreRequestDTO.StoreCreateRequestDTO dto, CustomUserDetails userDetails) {
+        User user = userRepository.findById(userDetails.getId()).orElseThrow(() -> new UserException(UserErrorCode.NOT_FOUND));
         if (!(user.getRole() == Role.MASTER || user.getRole() == Role.OWNER)) {
             throw new StoreException(StoreErrorCode.UNAUTHORIZED_ACCESS);
         }
-
         if (storeRepository.existsByName(dto.getName())) {
             throw new StoreException(StoreErrorCode.ALREADY_ADD);
         }
 
-        StoreCategory category = storeCategoryRepository.findById(dto.getStoreCategoryId())
-                .orElseThrow(() -> new StoreException(StoreErrorCode.NOT_FOUND));
-
-        Region region = regionRepository.findById(dto.getRegionId())
-                .orElseThrow(() -> new StoreException(StoreErrorCode.NOT_FOUND));
+        StoreCategory category = storeCategoryRepository
+                .findByCategory(dto.getCategory())
+                .orElseGet(() -> storeCategoryRepository.save(
+                        StoreCategoryConverter.toStoreCategory(dto.getCategory())
+                ));
+        Region region = regionRepository.findById(dto.getRegionId()).orElseThrow(() -> new StoreException(StoreErrorCode.NOT_FOUND));
 
         Store store = StoreConverter.toStore(dto);
         store.setStoreCategory(category);
         store.setRegion(region);
+        //TODO store의 address에 따라 region 생성하고 저장
         store.setUser(user);
         storeRepository.save(store);
-
         return StoreConverter.toStoreCreateResponseDTO(store);
     }
 
-    public StoreResponseDTO.StoreUpdateResponseDTO updateStore(UUID storeId, StoreRequestDTO.StoreUpdateRequestDTO dto, User user) {
+    public StoreResponseDTO.StoreUpdateResponseDTO updateStore(UUID storeId, StoreRequestDTO.StoreUpdateRequestDTO dto, CustomUserDetails userDetails) {
         Store store = storeRepository.findById(storeId)
                 .orElseThrow(() -> new StoreException(StoreErrorCode.NOT_FOUND));
+        User user = userRepository.findById(userDetails.getId()).orElseThrow(() -> new UserException(UserErrorCode.NOT_FOUND));
+        if (!(user.getRole() == Role.MASTER || user.getRole() == Role.OWNER) ||
+                !storeRepository.existsByStoreAndUser(storeId, user.getId())) {
+            throw new StoreException(StoreErrorCode.UNAUTHORIZED_ACCESS);
+        }
 
+        StoreCategory category = storeCategoryRepository
+                .findByCategory(dto.getCategory())
+                .orElseGet(() -> storeCategoryRepository.save(
+                        StoreCategoryConverter.toStoreCategory(dto.getCategory())
+                ));
         if (!store.getUser().getId().equals(user.getId())) {
             throw new StoreException(StoreErrorCode.UNAUTHORIZED_ACCESS);
         }
 
-        store.update(dto.getName(), dto.getAddress(), dto.getRating(), dto.getReviewCount());
-
+        store.update(dto.getName(), dto.getAddress());
+        store.setStoreCategory(category);
+        //TODO store의 address에 따라 region 생성하고 저장
         storeRepository.save(store);
 
-        return StoreResponseDTO.StoreUpdateResponseDTO.builder().build();
+        return StoreResponseDTO.StoreUpdateResponseDTO.from(store);
     }
 
-    public void deleteStore(UUID storeId, User user) {
+    public void deleteStore(UUID storeId, CustomUserDetails userDetails) {
         Store store = storeRepository.findById(storeId)
                 .orElseThrow(() -> new StoreException(StoreErrorCode.NOT_FOUND));
-
-        if (!store.getUser().getId().equals(user.getId())) {
+        User user = userRepository.findById(userDetails.getId()).orElseThrow(() -> new UserException(UserErrorCode.NOT_FOUND));
+        if (!(user.getRole() == Role.MASTER || user.getRole() == Role.OWNER) ||
+                !storeRepository.existsByStoreAndUser(storeId, user.getId())) {
             throw new StoreException(StoreErrorCode.UNAUTHORIZED_ACCESS);
         }
-
         store.softDelete();
+        storeRepository.save(store);
     }
 }
