ea86f0faef78b562fcafa9663b9a556b4fdc36fb
JUNE <jh010303@naver.com>
2025-08-02 17:25:59 +0900

    :sparkles: feat: 카테고리 이름으로 카테고리 찾는 함수 구현

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/store/repository/StoreCategoryRepository.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/store/repository/StoreCategoryRepository.java
@@ -2,8 +2,11 @@ package com.example.cloudfour.peopleofdelivery.domain.store.repository;
 
 import com.example.cloudfour.peopleofdelivery.domain.store.entity.StoreCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StoreCategoryRepository extends JpaRepository<StoreCategory, UUID> {
 
+    Optional<StoreCategory> findByCategory(String category);
 }
