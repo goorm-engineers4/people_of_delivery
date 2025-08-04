787d3da7f0da0e6c1ed0d647962704250871a7e8
shieun <shieunmo@gmail.com>
2025-08-04 17:51:13 +0900

    ✨ Feat : Toss 연동 설정 - application.yml 작성 - 환경변수 설정 - restclientconfig, tossapiclient 설정

---
+++ src/main/java/com/example/cloudfour/peopleofdelivery/global/config/RestClientConfig.java
@@ -0,0 +1,36 @@
+package com.example.cloudfour.peopleofdelivery.global.config;
+
+import com.example.cloudfour.peopleofdelivery.domain.payment.dto.TossErrorResponse;
+import com.example.cloudfour.peopleofdelivery.domain.payment.exception.PaymentException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
+
+import java.util.Base64;
+
+@Configuration
+public class RestClientConfig {
+
+    private static final String AUTHORIZATION_HEADER = HttpHeaders.AUTHORIZATION;
+    private static final String BASIC_AUTH_PREFIX = "Basic ";
+    private static final String EMPTY_SECRET_KEY_SUFFIX = ":";
+
+    @Value("${toss.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public RestClient tossRestClient(@Value("${toss.base-url}") String baseUrl) {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader(AUTHORIZATION_HEADER,
+                        BASIC_AUTH_PREFIX + Base64
+                                .getEncoder()
+                                .encodeToString((secretKey + EMPTY_SECRET_KEY_SUFFIX).getBytes()))
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
