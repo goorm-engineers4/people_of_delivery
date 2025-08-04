29318c6d0a48ded17dc28dede3e6795db944615f
JUNE <jh010303@naver.com>
2025-08-04 15:39:39 +0900

    :sparkles: feat: 사용자가 가진 주소가 맞는지 확인

--- src/main/java/com/example/cloudfour/peopleofdelivery/domain/user/repository/UserAddressRepository.java
+++ src/main/java/com/example/cloudfour/peopleofdelivery/domain/user/repository/UserAddressRepository.java
@@ -2,6 +2,8 @@ package com.example.cloudfour.peopleofdelivery.domain.user.repository;
 
 import com.example.cloudfour.peopleofdelivery.domain.user.entity.UserAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +15,6 @@ public interface UserAddressRepository extends JpaRepository<UserAddress, UUID> 
 
     Optional<UserAddress> findByIdAndUser_Id(UUID addressId, UUID userId);
 
+    @Query("select count(ua) > 0 from UserAddress ua where ua.user.isDeleted = false and ua.user.id =:userId and ua.id =:addressId")
+    boolean existsByUserIdAndAddressId(@Param("userId") UUID userId, @Param("addressId") UUID addressId);
 }
